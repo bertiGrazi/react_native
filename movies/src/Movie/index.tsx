b58dd851c3800b515207eb721f0dd0b2612c4825
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-04-08 17:29:03 -0300

    create app and create views

---
+++ movies/src/Movie/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export function Movie() {
+  return(
+    <View style={styles.container}>
+      <Text style={{color: "red"}}>Grazielli Lima Berti</Text>
+    </View>
+  )
+}
