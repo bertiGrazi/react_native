53b3a1b3abccb8042d6e7815018aacbea3bf2442
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-04-08 18:03:00 -0300

    finish cover component

--- movies/src/components/Cover/indx.tsx
+++ movies/src/components/Cover/indx.tsx
@@ -12,9 +12,11 @@ export function Cover() {
       colors={["rgba(0, 0, 0, 0.2)", "#000"]}
       style={styles.gratient}
       >
+        <Text style={styles.classification}>12</Text>
        <Text style={styles.title}>Harry Potter</Text>
+       <Text style={styles.type}>Ação, Aventura, Fantasia, Ficção</Text>
+       <Text style={styles.duration}>3 horas e 02 minutos</Text>
     </LinearGradient>
-    
     </ImageBackground>
 
 
--- movies/src/components/Cover/style.ts
+++ movies/src/components/Cover/style.ts
@@ -10,10 +10,33 @@ export const styles = StyleSheet.create({
     padding: 24,
     justifyContent: "flex-end"
   },
+  classification: {
+    color: "#FFFF",
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor: "#FE7300",
+    width: 32,
+    height: 32,
+    textAlignVertical: "center",
+    textAlign: "center",
+    paddingTop: 8,
+    borderRadius: 5,
+    marginBottom: 7
+  },
   title: {
     color: "#FFFFFF",
     fontSize: 22,
     fontWeight: "bold"
-  }
+  },
+  type: {
+    color: "#C4C4C4",
+    fontSize: 16,
+  },
+  duration: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    marginTop: 16
+  },
+  
 
 })
